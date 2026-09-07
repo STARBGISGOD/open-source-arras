68ec6e88dd0962c030bdd3ff776dbe9ce4e12b28
STARBGISGOD <surajnirala789@gmail.com>
2026-09-07 17:11:13 +0530

    Change spawn class from 'basic' to 'developer'

--- server/config.js
+++ server/config.js
@@ -274,7 +274,7 @@ module.exports = {
     bot_name_prefix: "[AI] ", // This is prefixed before the bot's randomly chosen name.
 
     // The class that players and bots spawn as.
-    spawn_class: 'basic',
+    spawn_class: 'developer',
 
     // How every entity regenerates their health.
     regenerate_tick: 100,
