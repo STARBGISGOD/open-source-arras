1caa21f5d6ffcf914b424c2cba0b8a1656a6b420
STARBGISGOD <surajnirala789@gmail.com>
2026-09-08 13:56:49 +0530

    Fix syntax error in room_setup array

--- server/config.js
+++ server/config.js
@@ -399,7 +399,7 @@ module.exports = {
     random_body_colors: false,
 
     // Room setup (don't change these unless you know what you're doing)
-    room_setup: ['room_default],
+    room_setup: ['room_default'],
     round_arena: false,
     mode: 'ffa',
 }
