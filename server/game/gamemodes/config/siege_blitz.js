6ec32d566a88548cb74d06d94f5ec710f16a30c4
STARBGISGOD <surajnirala789@gmail.com>
2026-09-09 18:44:35 +0530

    Reduce map and food capacities in siege_blitz mode

--- server/game/gamemodes/config/siege_blitz.js
+++ server/game/gamemodes/config/siege_blitz.js
@@ -3,10 +3,10 @@ module.exports = {
     teams: 1,
     siege: true,
     use_limited_waves: false,
-    map_tile_width: 420,
-    map_tile_height: 420,
+    map_tile_width: 220,
+    map_tile_height: 220,
     wave_cap: 100, // use_limited_waves: false to use this setting
-    food_cap: 40,
+    food_cap: 20,
     food_cap_nest: 5,
     room_setup: ["room_siege_blitz"],
     sanctuary_size: 19,
